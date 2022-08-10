26fd30c8db4afe790d34673fca4cdd9f40727c92
Paulina Szarek <xpaula20@wp.pl>
2022-08-10 19:50:20 +0200

    Edit and update ingredient

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -8,13 +8,31 @@ class IngredientsController < ApplicationController
   end
 
   def new
+    @ingredient = Ingredient.new
+  end
 
+  def edit
+    @ingredient = Ingredient.find(params[:id])
   end
 
   def create
     @ingredient = Ingredient.new(params.require(:ingredient).permit(:name, :amount, :unit))
-    @ingredient.save
+    if @ingredient.save
+      flash[:notice] = "Ingredient was created successfully."
     redirect_to @ingredient
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @ingredient = Ingredient.find(params[:id])
+    if @ingredient.update(params.require(:ingredient).permit(:name, :amount, :unit))
+      flash[:notice] = "Ingredient was updated successfully."
+      redirect_to @ingredient
+    else
+      render 'edit'
+    end
   end
 
 end
