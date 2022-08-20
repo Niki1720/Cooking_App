baa6433ad75ab866db52b18c3384032266696f2b
Paulina Szarek <xpaula20@wp.pl>
2022-08-20 15:39:05 +0200

    Add user password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_19_171515) do
+ActiveRecord::Schema.define(version: 2022_08_20_132420) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2022_08_19_171515) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
