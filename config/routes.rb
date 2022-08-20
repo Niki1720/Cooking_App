2c2a6fde4448dcd85df8b771664d0445c52559f4
Paulina Szarek <xpaula20@wp.pl>
2022-08-20 18:49:05 +0200

    Add users create action

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :ingredients
   resources :recipes
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
