1982bc056ab087b1334da424851d759cc850e6b4
Paulina Szarek <xpaula20@wp.pl>
2022-08-29 18:35:42 +0200

    Add admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_20_132420) do
+ActiveRecord::Schema.define(version: 2022_08_29_145326) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2022_08_20_132420) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
