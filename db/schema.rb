25dfafeb05a0e1c66e47c1b8202a9ef4d50aee8e
Paulina Szarek <xpaula20@wp.pl>
2022-09-18 22:57:18 +0200

    Destroy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_10_212244) do
+
+ActiveRecord::Schema.define(version: 2022_09_15_161336) do
 
   create_table "ingredient_names", force: :cascade do |t|
     t.string "name"
@@ -52,11 +53,20 @@ ActiveRecord::Schema.define(version: 2022_09_10_212244) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "password_digest"
     t.boolean "admin", default: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
